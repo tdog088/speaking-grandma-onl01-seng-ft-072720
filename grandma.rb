e9c9586f59c515c269574c45524157210c401a45
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-07-30 03:30:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
-  return "NO, NOT SINCE 1938!"
-end
+    return "NO, NOT SINCE 1938!"
+  end
 end
