11cc4bf1c46ad4b7fbe15c340516233e4155ecbd
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-07-30 03:21:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,4 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-  else 
-    return "NO, NOT SINCE 1938!"
-  end
 end
